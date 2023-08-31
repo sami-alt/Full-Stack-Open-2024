557d2af4b13bf23a3c00b8153ccf38ef615b47b0
sami-alt <goodfellatoo@gmail.com>
2023-08-31 13:48:26 +0300

    unicafe exercise ready.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,51 @@
 import { useState } from 'react'
 
 const Button = (props) => (
+  
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
-const Display = (props) => (
-  <div>{props.text} {props.value}</div>
-)
+const StatisticsLine = (props) => {
+  return (
+    <tr>
+      <td>
+        {props.text}
+      </td>
+      <td>
+        {props.value} {props.percent}
+      </td>
+    </tr>
+  )
+}
 
-const Statistics = () =>{
+const Statistics = (props) => {
+  const getNums = (props) => (props.good + props.neutral + props.bad)
+  const total = getNums(props)
+
+    if(getNums(props) === 0){
+      return(
         <div>
-        <Display  value={good} text='good'/>
-        <Display  value={neutral} text='neutral'/>
-        <Display  value={bad} text='bad'/>
-        <Display  value={total} text='all'/>
-        </div>
+          <h1>statistics</h1>
+          no feedback given
+          </div>
+      )
+    }
+  
+  return (
+    <div>
+      <table>
+        <tbody>
+          <StatisticsLine text='good' value={props.good} />
+          <StatisticsLine text='neutral' value={props.neutral} />
+          <StatisticsLine text='bad' value={props.bad} />
+          <StatisticsLine text='all' value={props.good + props.neutral + props.bad} />
+          <StatisticsLine text='average' value={(((1 * props.good) + (0 * props.neutral) + (-1 * props.bad)) / total).toFixed(1)} />
+          <StatisticsLine text='positive' value={((props.good /total)*100).toFixed(0)} percent='%' />
+        </tbody>
+      </table>
+    </div>
+  )
+
 }
 
 
@@ -22,18 +53,19 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total,setTotal] = useState(0)
+  const [total, setTotal] = useState(0)
 
   return (
     <div>
       <h1>give feedback</h1>
       <span>
-        <Button handleClick={()=>{setGood(good + 1); setTotal(total + 1)}} text='good'/>
-        <Button handleClick={()=>{setNeutral(neutral + 1); setTotal(total + 1)}} text='neutral'/>
-        <Button handleClick={()=>{setBad(bad + 1); setTotal(total + 1)}} text='bad'/>
+        <Button handleClick={() => { setGood(good + 1); setTotal(total + 1) }} text='good' />
+        <Button handleClick={() => { setNeutral(neutral + 1); setTotal(total + 1) }} text='neutral' />
+        <Button handleClick={() => { setBad(bad + 1); setTotal(total + 1) }} text='bad' />
       </span>
-      <h1>statistics</h1>
-        <Statistics/>
+      <div>
+        <Statistics good={good} bad={bad} neutral={neutral} />
+      </div>
     </div>
   )
 }
