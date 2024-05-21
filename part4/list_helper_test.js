b83f4a410e263c0c7ea33abe1bf27d4f897f16ab
Sami Räihä <sami.k.raiha@utu.fi>
2024-05-21 11:53:04 +0300

    exercises part4.5-7

--- part4/list_helper_test.js
+++ part4/list_helper_test.js
@@ -1,6 +1,7 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('./utils/list_helper')
+const { title } = require('node:process')
 
 
 test('dummy returns one', () => {
@@ -84,4 +85,88 @@ describe('total likes', () => {
         assert.strictEqual(result, 0)
     })
 
+})
+
+describe('Favorite blog, most blogs writen, most liked author',()=> {
+    const blogs = [
+        {
+            "title": "Testing blog applications",
+            "author": "Me Of Course",
+            "url": "www.internet.web",
+            "likes": 7,
+            "id": "664b9904501d6a9644429a4c"
+        },
+        {
+            "title": "Testing blog applications still",
+            "author": "You Of Course",
+            "url": "www.internet.fi",
+            "likes": 10,
+            "id": "664b991c501d6a9644429a4e"
+        },
+        {
+            "title": "Testing blog applications still and still",
+            "author": "You Of Course",
+            "url": "www.internet.com",
+            "likes": 1,
+            "id": "664b9935501d6a9644429a50"
+        },
+        {
+            "title": "Got bored of testing blog aplication",
+            "author": "You Of Course",
+            "url": "www.internet.to",
+            "likes": 3,
+            "id": "664b9958501d6a9644429a52"
+        },
+        {
+            "title": "Now i get paid to test blog aplication so i have to",
+            "author": "Her Of Course",
+            "url": "www.internet.net",
+            "likes": 12,
+            "id": "664b9991501d6a9644429a56"
+        },
+        {
+            "title": "Got fired from testing blog aplication so now i dont have to",
+            "author": "No one Of Course",
+            "url": "www.internet.de",
+            "likes": 3,
+            "id": "664b99e6501d6a9644429a58"
+        }
+    ]
+
+    const oneBlog = [
+        {
+        "title": "Testing blog applications still and still",
+        "author": "Him Of Course",
+        "url": "www.internet.com",
+        "likes": 1,
+        "id": "664b9935501d6a9644429a50"
+        }
+    ]
+
+    test('Favorite blog', ()=> {
+        const result = listHelper.favoriteBlog(blogs)
+        const correct = {
+            title: 'Now i get paid to test blog aplication so i have to',
+            author:'Her Of Course',
+            likes: 12
+        }
+        assert.deepStrictEqual(result, correct)
+    })
+
+    test('most blogs', ()=> {
+        const result = listHelper.mostBlogs(blogs)
+        const correct = {
+            author:'You Of Course',
+            blogs:3
+        }
+        assert.deepStrictEqual(result, correct)
+    })
+
+    test('Likes by most liked', ()=> {
+        const result = listHelper.mostLikes(blogs)
+        const correct = 
+            { author: 'You Of Course', likes: 14 }
+        
+        assert.deepStrictEqual(result, correct)
+    })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,16 +1,53 @@
+const _ = require('lodash')
+
+
 const dummy = (blogs) => {
     return 1
 }
 
 const totalLikes = (blogs) => {
     const likes = blogs.map(blog => blog.likes).reduce((sum, likes) => sum + likes, 0)
-    
     return blogs.length ? likes : 0
 }
 
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((favorite, blog)=> favorite.likes < blog.likes ? blog :  favorite)
+    
+    const info = {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
 
+    }
+    return info
+}
+
+const mostBlogs = (blogs) => {
+    const grouped = _.groupBy(blogs, 'author')
+    const biggestGroup = _.maxBy(Object.values(grouped), 'length')
+
+    return {
+        author: biggestGroup[0].author,
+        blogs: biggestGroup.length,
+    }
+}
+
+const mostLikes = (blogs) => {
+    const likesByAuthor = Object.entries(_.groupBy(blogs, 'author'))
+    .map(blog => {
+        const author = blog[0]
+        const totalLikes = _.sumBy(blog[1], 'likes')
+        return {author, likes:totalLikes}
+    })
+    const mostLikedAuthor = _.maxBy(likesByAuthor, 'likes')
+
+    return mostLikedAuthor
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostLikes,
+    mostBlogs
 }
